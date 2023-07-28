b5b9743731ff67d564bda18192e7077c199eb043
YAGolubev <yagolubev@inbox.ru>
2023-07-28 23:06:50 +0300

    Add city in database from main form

--- project/weather/views.py
+++ project/weather/views.py
@@ -2,17 +2,24 @@ import requests
 from decouple import config
 from django.shortcuts import render
 
+from .forms import CityForm
 from .models import City
 
 
 def index(request):
+    if request.method == 'POST':
+        form = CityForm(request.POST)
+        form.save()
+
+    form = CityForm()
+
     url = (
         'https://api.openweathermap.org/data/2.5/weather'
         '?q={}&units=metric&lang=ru&appid=' + config('OPENWEATHER_APPID')
     )
 
     all_cities = []
-    cities = City.objects.all()[:3]
+    cities = City.objects.all()
     for city in cities:
         res = requests.get(url.format(city.name)).json()
         city_info = {
@@ -24,6 +31,7 @@ def index(request):
         all_cities.append(city_info)
 
     context = {
+        'form': form,
         'all_info': all_cities,
         'alert_variant': [
             'primary',
